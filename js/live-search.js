888188364f991def8b910dec29fa63edebfee8ac
scottbasgaard <sbasgaard@gmail.com>
2014-04-24 14:04:06 +0200

    Add key up / key down to live search

--- js/live-search.js
+++ js/live-search.js
@@ -21,18 +21,58 @@ jQuery(document).ready(function($) {
 
 	$("input#live-search").live("keyup", function(e) {
 		
-		clearTimeout($.data(this, 'timer'));
-		var search_string = $(this).val();
+		if ( e.keyCode != 40 && e.keyCode != 38 && e.keyCode != 13 ) {
+		
+			clearTimeout($.data(this, 'timer'));
+			var search_string = $(this).val();
 
-		// Do Search!
-		if (search_string == '') {
-			$("#live-search-results").fadeOut();
-			$('#live-search-text').fadeOut();
-		} else {
-			$("#live-search-results").fadeIn();
-			$('#live-search-text').fadeIn();
-			$(this).data('timer', setTimeout(search, 100));
-		};
+			// Do Search!
+			if (search_string == '') {
+				$("#live-search-results").fadeOut();
+				$('#live-search-text').fadeOut();
+			} else {
+				$("#live-search-results").fadeIn();
+				$('#live-search-text').fadeIn();
+				$(this).data('timer', setTimeout(search, 100));
+			};
+		
+		}
+		
+	});
+	
+	var chosenResult = "";
+	
+	$("input#live-search").focus().keydown(function(e) {
+		
+		// Enter
+		if ( e.keyCode == 13 ) {
+			document.location.href = $('#live-search-results li.selected a').attr('href');
+			return false;
+		}
+		
+		// Key Down
+	    if ( e.keyCode == 40 ) { 
+	        if (chosenResult === "") {
+	            chosenResult = 0;
+	        } else if ((chosenResult+1) < $('#live-search-results li').length) {
+	            chosenResult++; 
+	        }
+	        $('#live-search-results li').removeClass('selected');
+	        $('#live-search-results li:eq('+chosenResult+')').addClass('selected');
+	        return false;
+	    }
+		
+		// Key Up
+	    if ( e.keyCode == 38 ) { 
+	        if (chosenResult === "") {
+	            chosenResult = 0;
+	        } else if (chosenResult > 0) {
+	            chosenResult--;            
+	        }
+	        $('#live-search-results li').removeClass('selected');
+	        $('#live-search-results li:eq('+chosenResult+')').addClass('selected');
+	        return false;
+	    }
 	});
 
 });
